96845c96fd65cae256c40707f7511c285d52a634
Paul T. Bauman <ptbauman@gmail.com>
2018-12-19 11:23:38 -0500

    Add compute_solid_derivs function

--- src/physics/src/immersed_boundary.C
+++ src/physics/src/immersed_boundary.C
@@ -659,6 +659,79 @@ namespace GRINS
   }
 
   template<typename SolidMech>
+  void ImmersedBoundary<SolidMech>::compute_solid_derivs(const MultiphysicsSystem & system,
+                                                         AssemblyContext & solid_context,
+                                                         libMesh::FEMContext & fluid_context,
+                                                         unsigned int sqp,
+                                                         libMesh::Real delta,
+                                                         const libMesh::dof_id_type fluid_elem_id,
+                                                         const std::vector<libMesh::Point> & solid_qpoints,
+                                                         libMesh::DenseSubVector<libMesh::Number> & solid_coeff,
+                                                         libMesh::DenseSubVector<libMesh::Number> & Fufp,
+                                                         libMesh::DenseSubVector<libMesh::Number> & Fvfp,
+                                                         libMesh::DenseSubVector<libMesh::Number> & Fusp,
+                                                         libMesh::DenseSubVector<libMesh::Number> & Fvsp,
+                                                         libMesh::DenseSubVector<libMesh::Number> & Fulmp,
+                                                         libMesh::DenseSubVector<libMesh::Number> & Fvlmp,
+                                                         libMesh::DenseSubVector<libMesh::Number> & Fufm,
+                                                         libMesh::DenseSubVector<libMesh::Number> & Fvfm,
+                                                         libMesh::DenseSubVector<libMesh::Number> & Fusm,
+                                                         libMesh::DenseSubVector<libMesh::Number> & Fvsm,
+                                                         libMesh::DenseSubVector<libMesh::Number> & Fulmm,
+                                                         libMesh::DenseSubVector<libMesh::Number> & Fvlmm,
+                                                         libMesh::DenseSubMatrix<libMesh::Number> & Kuf,
+                                                         libMesh::DenseSubMatrix<libMesh::Number> & Kvf,
+                                                         libMesh::DenseSubMatrix<libMesh::Number> & Kus,
+                                                         libMesh::DenseSubMatrix<libMesh::Number> & Kvs,
+                                                         libMesh::DenseSubMatrix<libMesh::Number> & Kulm,
+                                                         libMesh::DenseSubMatrix<libMesh::Number> & Kvlm)
+  {
+    this->zero_residuals(Fusp,Fvsp,Fulmp,Fvlmp,Fufp,Fvfp);
+    this->zero_residuals(Fusm,Fvsm,Fulmm,Fvlmm,Fufm,Fvfm);
+
+    unsigned int n_solid_dofs = solid_context.get_dof_indices(this->_disp_vars.u()).size();
+    unsigned int n_fluid_dofs = fluid_context.get_dof_indices(this->_flow_vars.u()).size();
+    unsigned int n_lambda_dofs = solid_context.get_dof_indices(this->_lambda_var.u()).size();
+
+    for( unsigned int j = 0; j < n_solid_dofs; j++ )
+      {
+        solid_coeff(j) += delta;
+        this->prepare_fluid_context(system,solid_context,solid_qpoints,sqp,fluid_elem_id,fluid_context);
+
+        this->compute_residuals(solid_context,fluid_context,sqp,
+                                Fufp,Fvfp,Fusp,Fvsp,Fulmp,Fvlmp);
+
+        solid_coeff(j) -= 2*delta;
+        this->prepare_fluid_context(system,solid_context,solid_qpoints,sqp,fluid_elem_id,fluid_context);
+
+        this->compute_residuals(solid_context,fluid_context,sqp,
+                                Fufm,Fvfm,Fusm,Fvsm,Fulmm,Fvlmm);
+
+        solid_coeff(j) += delta;
+        // IS THIS NEEDED?
+        this->prepare_fluid_context(system,solid_context,solid_qpoints,sqp,fluid_elem_id,fluid_context);
+
+        for (unsigned int i=0; i != n_fluid_dofs; i++)
+          {
+            Kuf(i,j) += (Fufp(i) - Fufm(i))/(2*delta);
+            Kvf(i,j) += (Fvfp(i) - Fvfm(i))/(2*delta);
+          }
+
+        for (unsigned int i=0; i != n_solid_dofs; i++)
+          {
+            Kus(i,j) += (Fusp(i) - Fusm(i))/(2*delta);
+            Kvs(i,j) += (Fvsp(i) - Fvsm(i))/(2*delta);
+          }
+
+        for (unsigned int i=0; i != n_lambda_dofs; i++)
+          {
+            Kulm(i,j) += (Fulmp(i) - Fulmm(i))/(2*delta);
+            Kvlm(i,j) += (Fvlmp(i) - Fvlmm(i))/(2*delta);
+          }
+      }
+  }
+
+  template<typename SolidMech>
   void ImmersedBoundary<SolidMech>::zero_residuals(libMesh::DenseSubVector<libMesh::Number> & Fuf,
                                                    libMesh::DenseSubVector<libMesh::Number> & Fvf,
                                                    libMesh::DenseSubVector<libMesh::Number> & Fus,
