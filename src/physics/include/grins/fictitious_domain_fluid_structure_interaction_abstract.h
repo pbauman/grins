e22dfbafc806968f63eba9b8b1ae583d6e93e85e
Paul T. Bauman <ptbauman@gmail.com>
2019-05-07 06:33:08 -0400

    Add FictitiousDomainFluidStructureInteractionAbstract::get_prev_time_solution
    MEthod to extract element vector for U_{n-1}

--- src/physics/include/grins/fictitious_domain_fluid_structure_interaction_abstract.h
+++ src/physics/include/grins/fictitious_domain_fluid_structure_interaction_abstract.h
@@ -240,6 +240,10 @@ namespace GRINS
                                  libMesh::DenseMatrix<libMesh::Number> & Klm_f,
                                  libMesh::DenseMatrix<libMesh::Number> & Kf_lm );
 
+    void get_prev_time_elem_solution( AssemblyContext & solid_context,
+                                      libMesh::DenseVector<libMesh::Number> & prev_time_solution ) const;
+
+
     template<unsigned int Dim, bool UseOldDisplacement>
     void compute_displaced_point( const MultiphysicsSystem & system,
                                   AssemblyContext & solid_context,
