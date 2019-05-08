e49283c1fe65abeb84f802146d1a5b1ec230bddf
Paul T. Bauman <ptbauman@gmail.com>
2019-05-08 18:25:15 -0400

    Add SemiImplicitCartesianFictitiousDomainFluidStructureInteraction

---
+++ src/physics/include/grins/semi_implicit_cartesian_fictitious_domain_fluid_structure_interaction.h
@@ -0,0 +1,55 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// GRINS - General Reacting Incompressible Navier-Stokes
+//
+// Copyright (C) 2014-2019 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+#ifndef GRINS_SEMI_IMPLICIT_CARTESIAN_FICTITIOUS_DOMAIN_FLUID_STRUCTURE_INTERACTION_H
+#define GRINS_SEMI_IMPLICIT_CARTESIAN_FICTITIOUS_DOMAIN_FLUID_STRUCTURE_INTERACTION_H
+
+// GRINS
+#include "grins/cartesian_fictitious_domain_fluid_structure_interaction_base.h"
+
+namespace GRINS
+{
+  template<unsigned int Dim>
+  class SemiImplicitCartesianFictitiousDomainFluidStructureInteraction :
+    public CartesianFictitiousDomainFluidStructureInteractionBase<Dim,true>
+  {
+  public:
+
+    SemiImplicitCartesianFictitiousDomainFluidStructureInteraction( const PhysicsName & physics_name,
+                                                                    const GetPot & input )
+      : CartesianFictitiousDomainFluidStructureInteractionBase<Dim,true>(physics_name,input)
+    {}
+
+    SemiImplicitCartesianFictitiousDomainFluidStructureInteraction() = delete;
+
+    virtual ~SemiImplicitCartesianFictitiousDomainFluidStructureInteraction() = default;
+
+    //! Cache mesh information needed for residual computation
+    virtual void presolve( MultiphysicsSystem & system ) override
+    { this->reinit_overlapping_data(system,true); }
+
+  };
+
+} // end namespace GRINS
+#endif // GRINS_SEMI_IMPLICIT_CARTESIAN_FICTITIOUS_DOMAIN_FLUID_STRUCTURE_INTERACTION_H
